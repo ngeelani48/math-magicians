20d4eaf9fe27ea9983118b7fcfbc9b05c8e08983
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-21 22:29:37 +0300

    Adjust Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Calculator from './components/Calculator';
-
+// Calculator is child component
 const App = () => <Calculator />;
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,22 +7,22 @@ const Calculator = () => (
       <button className="calc-button" type="button">AC</button>
       <button className="calc-button" type="button"> +/- </button>
       <button className="calc-button" type="button"> % </button>
-      <button className="calc-button right" type="button"> ÷ </button>
+      <button className="calc-button-right" type="button"> ÷ </button>
       <button className="calc-button" type="button"> 7 </button>
       <button className="calc-button" type="button"> 8 </button>
       <button className="calc-button" type="button"> 9 </button>
-      <button className="calc-button right" type="button"> x </button>
+      <button className="calc-button-right" type="button"> x </button>
       <button className="calc-button" type="button"> 4 </button>
       <button className="calc-button" type="button"> 5 </button>
       <button className="calc-button" type="button"> 6 </button>
-      <button className="calc-button right" type="button"> - </button>
+      <button className="calc-button-right" type="button"> - </button>
       <button className="calc-button" type="button"> 1 </button>
       <button className="calc-button" type="button"> 2 </button>
       <button className="calc-button" type="button"> 3 </button>
-      <button className="calc-button right" type="button"> + </button>
+      <button className="calc-button-right" type="button"> + </button>
       <button className="calc-button" id="zero" type="button"> 0 </button>
       <button className="calc-button" type="button"> . </button>
-      <button className="calc-button right" type="button"> = </button>
+      <button className="calc-button-right" type="button"> = </button>
     </div>
   </div>
 );
