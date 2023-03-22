005e715dc14c010c14f5a7842252db9ca0afcdae
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-22 08:06:08 +0300

    Add child component to Calculator.js

---
+++ src/components/CalcDisplay.js
@@ -0,0 +1,30 @@
+import './Calculator.css';
+
+const CalcDisplay = () => (
+  <div className="container">
+    <div className="result-display">0</div>
+    <div className="calc-buttons">
+      <button className="calc-button" type="button">AC</button>
+      <button className="calc-button" type="button"> +/- </button>
+      <button className="calc-button" type="button"> % </button>
+      <button className="calc-button-right" type="button"> ÷ </button>
+      <button className="calc-button" type="button"> 7 </button>
+      <button className="calc-button" type="button"> 8 </button>
+      <button className="calc-button" type="button"> 9 </button>
+      <button className="calc-button-right" type="button"> x </button>
+      <button className="calc-button" type="button"> 4 </button>
+      <button className="calc-button" type="button"> 5 </button>
+      <button className="calc-button" type="button"> 6 </button>
+      <button className="calc-button-right" type="button"> - </button>
+      <button className="calc-button" type="button"> 1 </button>
+      <button className="calc-button" type="button"> 2 </button>
+      <button className="calc-button" type="button"> 3 </button>
+      <button className="calc-button-right" type="button"> + </button>
+      <button className="calc-button" id="zero" type="button"> 0 </button>
+      <button className="calc-button" type="button"> . </button>
+      <button className="calc-button-right" type="button"> = </button>
+    </div>
+  </div>
+);
+
+export default CalcDisplay;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,10 @@
-import './Calculator.css';
+import React from 'react'
+import CalcDisplay from './CalcDisplay'
 
-const Calculator = () => (
-  <div className="container">
-    <div className="result-display">0</div>
-    <div className="calc-buttons">
-      <button className="calc-button" type="button">AC</button>
-      <button className="calc-button" type="button"> +/- </button>
-      <button className="calc-button" type="button"> % </button>
-      <button className="calc-button-right" type="button"> ÷ </button>
-      <button className="calc-button" type="button"> 7 </button>
-      <button className="calc-button" type="button"> 8 </button>
-      <button className="calc-button" type="button"> 9 </button>
-      <button className="calc-button-right" type="button"> x </button>
-      <button className="calc-button" type="button"> 4 </button>
-      <button className="calc-button" type="button"> 5 </button>
-      <button className="calc-button" type="button"> 6 </button>
-      <button className="calc-button-right" type="button"> - </button>
-      <button className="calc-button" type="button"> 1 </button>
-      <button className="calc-button" type="button"> 2 </button>
-      <button className="calc-button" type="button"> 3 </button>
-      <button className="calc-button-right" type="button"> + </button>
-      <button className="calc-button" id="zero" type="button"> 0 </button>
-      <button className="calc-button" type="button"> . </button>
-      <button className="calc-button-right" type="button"> = </button>
-    </div>
-  </div>
-);
+const Calculator = () => {
+  return (
+    <CalcDisplay></CalcDisplay>
+  )
+}
 
-export default Calculator;
+export default Calculator
