e71dd23213bc769c1edaf4100eb378d4026ebf08
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-22 23:11:49 +0300

    update calculator.js file for functionalities

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import CalcDisplay from './CalcDisplay';
+import React, { useState } from 'react';
+import './Calculator.css';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <CalcDisplay />
-);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleClick = (buttonName) => {
+    const data = calculate(state, buttonName);
+    setState(data);
+  };
+  return (
+    <div className="container">
+      <div className="result-display">{state.next || state.total || '0'}</div>
+      <div className="calc-buttons">
+        <Button value="AC" className="calc-button" handleClick={handleClick} />
+        <Button value="+/-" className="calc-button" handleClick={handleClick} />
+        <Button value="%" className="calc-button" handleClick={handleClick} />
+        <Button value="÷" className="calc-button-right" handleClick={handleClick} />
+        <Button value="7" className="calc-button" handleClick={handleClick} />
+        <Button value="8" className="calc-button" handleClick={handleClick} />
+        <Button value="9" className="calc-button" handleClick={handleClick} />
+        <Button value="x" className="calc-button-right" handleClick={handleClick} />
+        <Button value="4" className="calc-button" handleClick={handleClick} />
+        <Button value="5" className="calc-button" handleClick={handleClick} />
+        <Button value="6" className="calc-button" handleClick={handleClick} />
+        <Button value="-" className="calc-button-right" handleClick={handleClick} />
+        <Button value="1" className="calc-button" handleClick={handleClick} />
+        <Button value="2" className="calc-button" handleClick={handleClick} />
+        <Button value="3" className="calc-button" handleClick={handleClick} />
+        <Button value="+" className="calc-button-right" handleClick={handleClick} />
+        <Button value="0" className="calc-button-zero" handleClick={handleClick} />
+        <Button value="." className="calc-button" handleClick={handleClick} />
+        <Button value="=" className="calc-button-right" handleClick={handleClick} />
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
+
+const Button = ({ value, className, handleClick }) => (
+  <>
+    <button
+      type="button"
+      className={className}
+      onClick={() => handleClick(value)}
+    >
+      {value}
+    </button>
+  </>
+);
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
