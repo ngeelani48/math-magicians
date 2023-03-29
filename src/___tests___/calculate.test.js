76c36573e9029a81f95c46bea23b6d5188084329
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-03-29 11:53:27 +0300

    unit test for calculate.js

---
+++ src/___tests___/calculate.test.js
@@ -0,0 +1,65 @@
+import calculate from "../logic/calculate";
+
+describe('Calculate testing', ()=>{
+    it('Always an object should be returned', ()=>{
+        const type=typeof(calculate({},'AC'));
+        expect(type).toBe('object');
+    });
+    it('For AC buttonshould return an object with total, next and operation with null ', ()=>{
+        const result=(calculate({},'AC'));
+        expect(result).toEqual({
+            total: null,
+            next: null,
+            operation: null,
+          });
+    });
+    it('should add two numbers and return object', () => {
+        const input = { total: '4', next: '2', operation: '+' };
+        const output = { total: '6', next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should subtract two numbers and return object', () => {
+        const input = { total: '4', next: '2', operation: '-' };
+        const output = { total: '2', next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should multiply two numbers and return object', () => {
+        const input = { total: '4', next: '2', operation: 'x' };
+        const output = { total: '8', next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should devide two numbers and return object', () => {
+        const input = { total: '4', next: '2', operation: '÷' };
+        const output = { total: '2', next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should find modulo of two numbers and return object', () => {
+        const input = { total: '4', next: '2', operation: '%' };
+        const output = { total: '0', next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should through error if devided by 0', () => {
+        const input = { total: '4', next: '0', operation: '÷' };
+        const output = { total: "Can't divide by 0.", next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+      it('should through error if operation is modulo by zero', () => {
+        const input = { total: '4', next: '0', operation: '%' };
+        const output = { total: "Can't find modulo as can't divide by 0.", next: null, operation: null };
+    
+        expect(calculate(input, '=')).toEqual(output);
+      });
+
+});
